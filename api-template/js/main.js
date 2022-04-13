4c264c1c2451001fc85371d7052a3db981d1b411
elloveronika <ello.veronika92@gmail.com>
2022-04-12 18:10:20 -0700

    got array of names from pokename function

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,18 +1,19 @@
 //Example fetch using pokemonapi.co
-document.querySelector("button").addEventListener("click", getFetch);
+document.querySelector("button").addEventListener("click", getPokeNames);
 
 //create a function that handles displayling the sprites
 
 //function that will handle the og fetch
 
-async function getPokeSprites(pokemon) {
-  let url = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log("this is data inside pokesprites", data);
+async function getPokeSprites(pokeName) {
+  console.log("this is array of pokeNames inside pokeSprites", pokeName);
+  // let url = "https://pokeapi.co/api/v2/pokemon/" + ;
+  // let res = await fetch(url);
+  // let data = await res.json();
+  // console.log("this is data inside pokesprites", data);
 }
 
-async function getFetch() {
+async function getPokeNames() {
   const choice = document.querySelector("input").value.toLowerCase();
 
   document.querySelector(".pokeName").innerText = choice;
@@ -37,43 +38,17 @@ async function getFetch() {
 
   let evoBegin = dataPokeEvo.chain;
 
-  let initialEvo = [evoBegin.species.name];
+  let names = [evoBegin.species.name];
 
   let nextEvo = dataPokeEvo.chain.evolves_to;
 
-  const spreadEvolves = [...nextEvo];
-  console.log("this is spreadEvolves", spreadEvolves);
+  // const spreadEvolves = [...nextEvo];
+  // console.log("this is spreadEvolves", spreadEvolves);
 
   while (nextEvo.length > 0) {
-    initialEvo.push(spreadEvolves.map((poke) => poke.species.name));
+    names.push(...nextEvo.map((poke) => poke.species.name));
 
     nextEvo = nextEvo[0].evolves_to;
   }
-  console.log("this is initial evo", initialEvo);
-  // console.log("this is second evo", thirdEvoPoke);
-
-  // const urlForFirstSprites =
-  //   "https://pokeapi.co/api/v2/pokemon/" + firstEvoPoke;
-
-  // const firstEvoSpriteRes = await fetch(urlForFirstSprites);
-
-  // const firstEvoSpriteData = await firstEvoSpriteRes.json();
-
-  // document.querySelector(".firstEvo").src =
-  //   firstEvoSpriteData.sprites.front_shiny;
-
-  // const urlForSecSprites = "https://pokeapi.co/api/v2/pokemon/" + secEvoPoke;
-  // const secEvoSpriteRes = await fetch(urlForSecSprites);
-  // const secEvoSpriteData = await secEvoSpriteRes.json();
-
-  // document.querySelector(".secEvo").src = secEvoSpriteData.sprites.front_shiny;
-
-  // const urlForThirdSprites =
-  //   "https://pokeapi.co/api/v2/pokemon/" + thirdEvoPoke;
-  // const thirdEvoSpriteRes = await fetch(urlForThirdSprites);
-  // const thirdEvoSpriteData = await thirdEvoSpriteRes.json();
-
-  // document.querySelector(".thirdEvo").src =
-  //   thirdEvoSpriteData.sprites.front_shiny;
-  // );
+  console.log("this is names", names);
 }
